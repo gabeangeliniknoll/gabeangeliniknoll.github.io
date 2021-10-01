3f92e3cc3fd51e785599562e401bf1016d18ec3c
Gabriel J. Angelini-Knoll <gabe.angelini.knoll@gmail.com>
2021-10-01 12:56:59 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,9 +26,8 @@ const IndexPage = () => (
     <div clear="both"></div>
     <div style={{ float: `left`, maxWidth: `100%`, marginBottom: `1.45rem`}}>
     <h2>Current Teaching</h2>
-      <p><b>Pro-Seminar on Algebra - Symmetries</b><span> </span><a href="https://www.mi.fu-berlin.de/math/groups/top/Lehre/PS-S_Algebra_So21.html">Course Website.</a><span> </span></p>
-      <p><b>19219401 Higher Algebra </b><span> </span><a href="https://www.mi.fu-berlin.de/math/groups/top/Lehre/2021so_HigherAlgebra.html">Course Website.</a> Leading exercise sessions.</p>
-      <p><b>Forschungsseminar Geometrie und Topologie</b><span> </span><a href="https://www.mi.fu-berlin.de/math/groups/top/research/FS-2021So-geom.html">Course Website.</a></p>
+      <p><b>19244701 Higher Algebra II</b><span> </span><a href="https://www.mi.fu-berlin.de/math/groups/top/Lehre/2021ws_HigherAlgebra2.html">Course Website.</a>Co-teaching with Prof. Dr. Holger Reich</p>
+      <p><b>19223811 Forschungsmodul: Topologie "Cyclic Homology"</b><span> </span><a href="https://www.mi.fu-berlin.de/math/groups/top/Lehre/_Topologie_Seminare/S_FM_Topologie_ws2021.html">Course Website.</a></p>
     <h2>Contact</h2>
       <h3>Pronouns:</h3>
         <p>He/Him/His</p>
