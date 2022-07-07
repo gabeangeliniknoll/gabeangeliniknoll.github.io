cbfc171128e9a021eab208c88387136ffd31b94d
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2022-07-07 17:23:44 +0200

    minor update test

--- src/pages/mini2022.js
+++ src/pages/mini2022.js
@@ -9,7 +9,7 @@ const Mini = () => (
     <h1>Mini-Symposium: Advances in K-Theory, Symmetry, and Periodicity</h1>
     <div>
     <div style={{ float: `left`, maxWidth: `70%`, marginBottom: `1.45rem`}}>
-      <p>This Mini-Symposium is part of the <a href="https://www.mi.fu-berlin.de/dmv2022/">DMV Annual Meeting 2022</a> to be held on September 12-16 in Berlin, Germany.</p>
+      <p>This Mini-Symposium is part of the <a href="https://www.mi.fu-berlin.de/dmv2022/">DMV Annual Meeting 2022</a> to be held on September 12-16 in Berlin, Germany. The Mini-symposium will take place on September 16th from 10:30-12:30 and 15:30-17:30. </p>
       <h2>Speakers</h2>
           <ul>
             <li>Jack Morgan Davies, Utrecht University</li> 
