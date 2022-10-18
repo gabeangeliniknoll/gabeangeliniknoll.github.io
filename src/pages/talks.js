65690dd04af377fcd83726df843d2f42c447b163
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2022-10-18 07:48:11 +0200

    update talks

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -20,9 +20,13 @@ const Talks = () => (
     <h1>Talks</h1>
       <h2>Invited Seminar talks</h2>
         <h3>2022</h3>
+          <h4>October</h4>
+            <br><font size="3"><a href="https://www.math.sciences.univ-nantes.fr/GDR-Topologie/">GDR-Topologie</a>, Nantes Universit<span>&eacute;</span>, Nantes, France</font></br>
+          <h4>September</h4>
+            </br><font size="3">Apr<span>&egrave;</span>s-midi Parisienne de Topologie Alg<span>&eacute;</span>brique</a>, Universit<span>&eacute;</span> Sorbonne Paris Nord, </font></p>
           <h4>June</h4>
             <p>Homology of twisted G-rings<br>
-              </br><font size="3"><a href="https://www.math.univ-paris13.fr/laga/index.php/fr/ta/seminaires">Algebraic Topology Seminar</a>, University of Paris 13, </font></p>
+              </br><font size="3"><a href="https://www.math.univ-paris13.fr/laga/index.php/fr/ta/seminaires">Algebraic Topology Seminar</a>, Universit<span>&eacute;</span> Sorbonne Paris Nord, </font></p>
           <h4>March</h4>
             <p>Algebraic K-theory of elliptic cohomology<br>
               </br><font size="3"><a href="https://s.wayne.edu/echt/echt-research-seminar/">Electronic Computational Homotopy Theory Seminar</a></font></p>
