a6713b79dbdc9d52f582b88e9140688820c940df
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2022-12-02 11:48:44 +0100

    fixing bugs 3

---
+++ src/pages/nulllstellensatz.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Gdt22 = () => (
+  <Layout>
+    <SEO title="GdT on chromatic nullstellensatz" />
+    <h1>Groupe de Travail on the chromatic Nullstellensatz</h1>
+    <p> We are organizing Groupe de traivail on the paper <a href="https://arxiv.org/abs/2207.09929">The chromatic nullstellensatz</a> by Burkulund-Schland-Yuan at Université Sorbonne Paris Nord in 2022-2023.</p>
+      <h2>Talks</h2>
+          <h4>October 20th Introduction to the seminar by Christian Ausoni </h4>
+          <h4>November 24th  Spherical Witt vectors by Victor Saunier</h4>
+          <h4>December 1st Lubin-Tate theory by Jordan Levin</h4>
+          <h4>December 15th Detecting nilpotence by Gregory Ginot</h4>
+          <h4>Janurary 16th Examples of detecting nulpotence by Geoffroy Horel</h4>
+          <h4>Februaty 2nd The height zero case of the main theorem</h4>
+          <h4>Februaty 9th The main theorem</h4>
+  </Layout>
+)
+
+export default Talks
