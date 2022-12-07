946900d100f19eebdf22a3d087457a03e3266728
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2022-12-07 21:23:43 +0100

    update and fix typos 2

--- src/pages/cnull.js
+++ src/pages/cnull.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Cnull from "../static/gdt-nullstellensatz"
+import Cnull from "../static/gdt-nullstellensatz.pdf"
 
 const Research = () => (
   <Layout>
