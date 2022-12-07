c70c56ee5e281cd05bf2913b85e81dd25680b416
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2022-12-07 21:42:18 +0100

    update and fix typos 4

--- src/pages/cnull.js
+++ src/pages/cnull.js
@@ -8,15 +8,22 @@ const Research = () => (
   <Layout>
     <SEO title="Groupe de Travail: The chromatic Nullstellensatz" />
     <h2>Groupe de Travail on the chromatic Nullstellensatz</h2>
-    <p> We are organizing a Groupe de traivail on the paper <a href="https://arxiv.org/abs/2207.09929">The chromatic nullstellensatz</a> by Burklund-Schlank-Yuan at Université Sorbonne Paris Nord in 2022-2023. The rough syllabus of the seminar is [<PDF pdfFile={Cnull} displayText='here' />]</p>
+    <p>We are organizing a <PDF pdfFile={Cnull} displayText='Groupe de traivail' /> on the paper <a href="https://arxiv.org/abs/2207.09929">The chromatic nullstellensatz</a> by Burklund-Schlank-Yuan at Université Sorbonne Paris Nord in 2022-2023.</p>
       <h3>Talks</h3>
-        <p>October 20th <bf>Introduction to the seminar </bf> by Christian Ausoni </p>
-        <p>November 24th: <bf>Spherical Witt vectors</bf> by Victor Saunier</p>  
-        <p>December 4th: <bf>Lubin-Tate theory</bf> by Jordan Levin</p>
-        <p>December 15th: <bf>Detecting nilpotence</bf> by Gregory Ginot</p>
-        <p>Janurary 16th: <bf>Examples of detecting nulpotence</bf> by Geoffroy Horel</p>
-        <p>Februaty 2nd: <bf>The height zero case of the main theorem</bf></p>
-        <p>Februaty 9th: <bf>The main theorem</bf></p>
+        <h4>October 20th</h4>
+          <p>Introduction to the seminar by Christian Ausoni</p>
+        <h4>November 24th</h4> 
+          <p>Spherical Witt vectors by Victor Saunier</p>  
+        <h4>December 4th</h4> 
+          <p>Lubin-Tate theory by Jordan Levin</p>
+        <h4>December 15th</h4>
+          <p>Detecting nilpotence by Gregory Ginot</p>
+        <h4>Janurary 16th</h4>
+        <p>Examples of detecting nulpotence by Geoffroy Horel</p>
+        <h4>Februaty 2nd</h4>
+          <p>The height zero case of the main theorem</p>
+        <h4>Februaty 9th</h4>
+          <p>The main theorem</p>
   </Layout>
 )
 
