704168a55c68d407b5b906d24954e2cc25a8ad86
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2023-04-27 11:24:38 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = () => (
     <h1>Homepage</h1>
     <div>
     <div style={{ float: `left`, maxWidth: `65%`, marginBottom: `1.45rem`}}>
-    <p>I am a postdoctoral fellow in the <a href="https://www.math.univ-paris13.fr/laga/index.php/fr/">Laboratoire Analyse, G<span>&eacute;</span>om<span>&eacute;</span>trie et Applications</a> at <a href="https://www.univ-paris13.fr/">Universit<span>&eacute;</span> Sorbonne Paris Nord</a>. Previously, I was a postdoctoral researcher at the <a href="https://www.mi.fu-berlin.de/math/groups/top/members/index.html">Freie Universit<span>&#228;</span>t Berlin</a> and <a href="https://math.msu.edu">Michigan State University</a>. 
-      I earned my PhD at <a href="https://clas.wayne.edu/math">Wayne State University</a> under the direction of <a href="https://clasprofiles.wayne.edu/profile/dy8211">Andrew Salch</a>.</p>
+    <p>I am a postdoctoral fellow in the <a href="https://www.math.univ-paris13.fr/laga/index.php/fr/">Laboratoire Analyse, G<span>&eacute;</span>om<span>&eacute;</span>trie et Applications</a> at the Universit<span>&eacute;</span> Sorbonne Paris Nord. Previously, I was a postdoctoral researcher at the Freie Universit<span>&#228;</span>t Berlin and Michigan State University. 
+      I earned my PhD at Wayne State University under the direction of <a href="https://clasprofiles.wayne.edu/profile/dy8211">Andrew Salch</a>.</p>
     <p>I research interactions between algebraic topology, arithmetic, and geometry. Specifically, I am interested invariants of ring spectra, such as algebraic K-theory and Hochschild homology, as well as chromatic, equivariant, and motivic homotopy theory. </p>
 		<p>In addition to my research activity, I have had the opportunity to teach a broad range of courses, 
       mentor research projects and theses, and organize several conferences and seminars. For a more detailed description of my activities see my <CV />.</p>
