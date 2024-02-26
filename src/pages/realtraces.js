aa3c4a37e51472e9285df00cf7f0afc0eaf91e9a
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2024-02-26 13:24:25 +0100

    update

--- src/pages/realtraces.js
+++ src/pages/realtraces.js
@@ -1,5 +1,5 @@
 import React from "react"
-//import Layout from "../components/layout"
+import Layout from "../components/layout2"
 import SEO from "../components/seo"
 
 //import Knotes from "./knotes"
