e157cd21c24cae5af962508cc0ee5850edfd59c1
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2024-08-28 21:06:12 +0200

    update mentoring

---
+++ src/pages/mentoring.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Mentoring = () => (
+  <Layout>
+    <SEO title="Mentoring" />
+    <h1>Masters student thesis direction</h1>
+      <h3>Jingyuan Zhu</h3>
+       <h4>Title: Equivariant $\infty$-Categories and Real Topological Hochschild Homology</h4> 
+       <p>Co-supervised with Geoffroy Horel, Université Paris Saclay, 2024</p>
+      <h3>Lucas Piessevaux</h3>
+        <h4>Deformations of stable homotopy</h4>
+        <p>Freie Universit\"at Berlin, 2022</p>
+      <h3>Ferry Saavedra</h3>
+        <h4>On generalized Tate cohomology</h4>
+        <p>Co-supervised with Holger Reich, Freie Universit\"at Berlin, 2022.</p>
+      <h3>Daniel Krupa</h3>
+        <h4> Infty categories and K-theory</h4>
+        <p>Co-supervised with Holger Reich, Freie Universit\"at Berlin, 2021</p>
+    <h1>Undergraduate student thesis direction</h1>
+      <h3>Sebastian Schneider</h3>
+        <h4>Algebra via Lawverre theories</h4>
+        <p>Freie Universit\"at Berlin, 2022</p>
+      <h3>Vittorio Di Fraia</h3>
+        <h4>Variation on the Little Cubes Operads
+        and Involution Algebra Objects</h4>
+        <p>Freie Universit\"at Berlin, 2021</p>
+    <h1>Research Experiences for Undergraduates</h1>
+      <h4>SURIEM, nationwide REU for early career undergraduates</h4>
+        <h3>Project mentor for a project on gneralized Catalan numbers and action graphs</h3> 
+        <p>Michigan State University, 2019</p>
+      <h4>Mathematics Exchange Program, International REU</h4>  
+        <h3>Project mentor with Teena Gerhardt for a project on Coagbras.</h3>
+        <p>Michigan State University, 2019</p>
+  </Layout>
+)
+
+export default Mentoring
