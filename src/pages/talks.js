7ddb55abb695f5c97e4e03716b165a6571d89eb8
gabeangeliniknoll <gabe.angelini.knoll@gmail.com>
2024-12-17 10:50:17 +0100

    update

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -18,14 +18,16 @@ const Talks = () => (
   <Layout>
     <SEO title="Talks" />
     <h1>Invited Talks</h1>
-      <h2>Upcoming talks</h2>
-      <h3>December</h3>
-          <h4>TBD</h4>
-          <p><font size="3"><a href="https://warwick.ac.uk/fac/sci/maths/people/staff/dotto/venice24/">Real THH in Venice</a>, Venice, Italy</font></p>
-      <h3>October</h3>
-          <h4>TBD</h4>
-          <p><font size="3"><a href="https://sites.google.com/view/nialltaggartmath/seminars-events/topics">Topology Intercity Seminar</a>, Radboud University, Nijmegen</font></p>
       <h2>2024</h2>
+        <h3>December</h3>
+          <h4>Real syntomic cohomology</h4>
+          <p><font size="3"><a href="https://warwick.ac.uk/fac/sci/maths/people/staff/dotto/venice24/">Real THH in Venice</a>, Venice, Italy</font></p>
+        <h3>December</h3>
+          <h4>Algebraic K-theory of minimal p-local skew-fields in homotopy theory</h4>
+          <p><font size="3"><a href="https://az.uni-wuppertal.de/en/activities/guest-seminar">Algebra and Topology Seminar</a>, University of Wupertal</font></p>
+        <h3>October</h3>
+          <h4>Algebraic K-theory of minimal p-local skew-fields in homotopy theory</h4>
+          <p><font size="3"><a href="https://sites.google.com/view/nialltaggartmath/seminars-events/topics">Topology Intercity Seminar</a>, Radboud University, Nijmegen</font></p>
         <h3>July</h3>
           <h4>Syntomic cohomology of ring spectra</h4>
           <p><font size="3"><a href="https://www.mfo.de/occasion/2430/www_view">Topologie</a>, Oberwolfach.</font></p>
